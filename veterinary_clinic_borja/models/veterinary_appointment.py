fcc32070e9c6ff65b72c4bdec97e546394d58852
Borja <borja@somconnexio.coop>
2024-11-11 17:26:22 +0000

    feat: add annotations

--- veterinary_clinic_borja/models/veterinary_appointment.py
+++ veterinary_clinic_borja/models/veterinary_appointment.py
@@ -3,6 +3,8 @@ from odoo import fields, models
 class VeterinaryAppointment(models.Model):
     _name = "veterinary.appointment"
     _description = "Veterinary Appointment"
+    # Name by defect is reason
+    # _rec_name = "reason"
 
     date = fields.Datetime(string="Date", required=True)
     reason = fields.Char(string="Reason", required=True)
@@ -11,5 +13,5 @@ class VeterinaryAppointment(models.Model):
         ("done", "Done"),
         ("cancelled", "Cancelled"),
         ("other", "Other")   
-    ])
+    ], default="draft", string="Status")
     duration_minutes = fields.Integer(string="Duration", required=True, help="Duration in minutes")
