ce3f9a8329e23b5aa5a7828cac861df2e0139c2e
Borja <borja@somconnexio.coop>
2024-11-27 15:12:19 +0000

    feat: wizard redirect to view

--- veterinary_clinic_borja/wizards/veterinary_surgery_edit_status.py
+++ veterinary_clinic_borja/wizards/veterinary_surgery_edit_status.py
@@ -14,5 +14,17 @@ class VeterinarySurgeryEditStatus(models.TransientModel):
 
     def mass_edit_status(self):
         active_ids = self.env.context.get("active_ids")
+        if not active_ids:
+             active_ids = self.env["veterinary.appointment"].search([])
         surgeries = self.env["veterinary.surgery"].browse(active_ids)
         surgeries.write({"status": self.status})
+        context = dict(self.env.context)
+        context.update({"default_status": "draft"})
+        return {
+         "type": "ir.actions.act_window",
+         "name": "Ventana Nueva",
+         "res_model": "veterinary.surgery.edit.status",
+         "view_mode": "form",
+         "target": "new",
+         "context": context,
+      }
