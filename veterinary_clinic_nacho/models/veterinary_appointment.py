f82f009ff237e9352b45d86e5495347b52f4e31d
Borja <borja@somconnexio.coop>
2024-12-02 16:12:34 +0000

    fix: nacho conflicts

--- veterinary_clinic_nacho/models/veterinary_appointment.py
+++ veterinary_clinic_nacho/models/veterinary_appointment.py
@@ -1,21 +1,104 @@
-from odoo import models, fields
-
+from odoo import models, fields, api, Command
+from odoo.exceptions import ValidationError
 
 class VeterinaryAppointment(models.Model):
-    _name = "veterinary.appointment"
-    _description = "Veterinary Appointment"
-    _rec_name = "reason"
+    _name = 'veterinary.appointment'
+    _description = 'Veterinary Appointment'
 
-    date = fields.Datetime(
-        string="Date",
-        required=True,
-        help="Date of the appointment",
-        default=fields.Datetime.now,
-    )
-    reason = fields.Text(string="Reason", help="Reason for the appointment")
-    state = fields.Selection(
-        [("draft", "Draft"), ("done", "Done"), ("cancel", "Cancel")],
-        default="draft",
-        string="State",
-    )
-    duration = fields.Float(string="Duration")
+    name = fields.Char(string='Name', required=True, copy=False, default='New')
+    partner_id = fields.Many2one('res.partner', string='Partner')
+    pet_id = fields.Many2one('veterinary.pet', string='Pet')
+    partner_phone = fields.Char(string='Phone')
+    partner_email = fields.Char(string='Email', related='partner_id.email', store=True, readonly=False)
+    date = fields.Datetime(string='Date', required=True, help='Date of the appointment', default=fields.Datetime.now)
+    reason = fields.Text(string='Reason', help='Reason for the appointment')
+    solution = fields.Html(string='Solution', help='Solution to the problem')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Done'),
+        ('cancel', 'Cancel')
+    ], default='draft', string='State', group_expand='_group_expand_states')
+    duration = fields.Float(string='Duration')
+    user_id = fields.Many2one('res.users', string='Responsible', default=lambda self: self.env.user)
+    sequence = fields.Integer(string='Sequence', default=10)
+    urgency = fields.Boolean(string='Urgent')
+    color = fields.Integer(string='Color', company_dependent=True)
+    assigned = fields.Boolean(string='Assigned')
+    tag_ids = fields.Many2many('veterinary.appointment.tag', string='Tags')
+    line_ids = fields.One2many('veterinary.appointment.line', 'appointment_id', string='Lines')
+    total = fields.Monetary(string='Total', compute='_compute_total', store=True)
+    currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
+
+    _sql_constraints = [
+        ('name_unique', 'unique (name)', "The appointment name must be unique"),]
+    
+    # @api.model
+    # def create(self, vals):
+    #     vals['name'] = 'Nuevo/loquesea'
+    #     res = super().create(vals)
+    #     return res
+
+    @api.onchange('assigned')
+    def _onchange_assigned(self):
+        if self.assigned:
+            self.user_id = self.env.user
+        else:
+            self.user_id = False
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.partner_phone = self.partner_id.phone or self.partner_id.mobile
+        else:
+            self.partner_phone = False
+
+    @api.constrains('duration')
+    def _check_duration(self):
+        for record in self:
+            if record.duration < 0:
+                raise ValidationError('The duration must be greater than zero')
+
+    @api.depends('line_ids.subtotal')
+    def _compute_total(self):
+        for record in self:
+            record.total = sum(record.line_ids.mapped('subtotal'))
+
+    def _inverse_assigned(self):
+        for record in self:
+            if record.assigned:
+                record.user_id = self.env.user
+            else:
+                record.user_id = False
+
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = bool(record.user_id)
+
+    def action_draft(self):
+        for record in self:
+            record.state = 'draft'
+
+    def action_done(self):
+        import pdb;pdb.set_trace()
+        for record in self:
+            record.state = 'done'
+
+    def action_cancel(self):
+        for record in self:
+            record.state = 'cancel'
+    
+    def _group_expand_states(self, states, domain, order):
+        return [key for key, val in type(self).state.selection]
+    
+    def create_tags(self):
+        tag_ids = self.env['veterinary.appointment.tag'].search([('name','ilike', self.reason)])
+        # Si quisiera añadir las etiquetas existentes podría usar el siguiente operador:
+        # tag_ids |= self.tag_ids
+        if tag_ids:
+            self.tag_ids = [Command.set(tag_ids.ids)]
+            #self.tag_ids = [(6, 0, self.tag_ids.ids)]
+        else:
+            self.tag_ids = [Command.create({'name': self.reason})]
+            #self.tag_ids = [(0,0, {'name': self.reason})]
+            
--- veterinary_clinic_nacho/models/veterinary_pet.py
+++ veterinary_clinic_nacho/models/veterinary_pet.py
@@ -1,15 +1,126 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+import random
+from datetime import date
 
 class VeterinaryPet(models.Model):
-    _name = "veterinary.pet"
-    _description = "Veterinary Pet"
+    _name = 'veterinary.pet'
+    _description = 'Veterinary Pet'
 
-    name = fields.Char(string="Name", required=True, help="Name of the pet")
-    birthdate = fields.Date(string="Birthdate", help="Birthdate of the pet")
-    weight = fields.Float(string="Weight")
-    age = fields.Integer(string="Age")
-    pet_number = fields.Char(string="Pet Number", help="Number of the pet")
-    species = fields.Selection(
-        [("cat", "Cat"), ("dog", "Dog"), ("bird", "Bird"), ("other", "Other")]
-    )
+    name = fields.Char(string='Name', required=True, help='Name of the pet')
+    active = fields.Boolean(string='Active', default=True)
+    birthdate = fields.Date(string='Birthdate', help='Birthdate of the pet')
+    weight = fields.Float(string='Weight')
+    age = fields.Integer(string='Age', compute='_compute_age', search='_search_age')
+    pet_number = fields.Char(string='Pet Number', help='Number of the pet', copy=False)
+    species_id = fields.Many2one('veterinary.species', string='Species', help='Species of the pet')
+    vaccinated = fields.Boolean(string='Vaccinated', compute="_compute_vaccinated", inverse="_inverse_vaccinated")
+    last_vaccination_date = fields.Date(string='Last Vaccination Date')
+    photo = fields.Image(string='Photo')
+    allergy_ids = fields.Many2many('veterinary.allergy', string='Allergies')
+    adopted = fields.Boolean(string='Adopted', help='Adopted pet')
+    appointment_ids = fields.One2many('veterinary.appointment', 'pet_id', string='Appointments')
+    appointment_count = fields.Integer(string='Appointment Count', compute='_compute_appointment_count')
+    insurance_ids = fields.One2many('veterinary.insurance', 'pet_id', string='Insurances')
+    insurance_count = fields.Integer(string='Insurance Count', compute='_compute_insurance_count')
+    surgery_count = fields.Integer(string='Surgery Count', compute='_compute_surgery_count')
+
+    _sql_constraints = [
+        ('number_species_unique', 'unique(pet_number,species_id)', 'The pet number must be unique per species!'),]
+
+    def _compute_surgery_count(self):
+        for record in self:
+            surgeries = self.env['veterinary.surgery'].search([('pet_id', '=', record.id)])
+            record.surgery_count = len(surgeries)
+
+    def action_view_surgeries(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Surgeries',
+            'res_model': 'veterinary.surgery',
+            'view_mode': 'tree,form',
+            'domain': [('pet_id', '=', self.id)],
+        }
+
+    def _compute_insurance_count(self):
+        for record in self:
+            record.insurance_count = len(record.insurance_ids)
+
+    def action_view_insurances(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Insurances',
+            'res_model': 'veterinary.insurance',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', self.insurance_ids.ids)],
+        }
+
+    def _compute_appointment_count(self):
+        for record in self:
+            record.appointment_count = len(record.appointment_ids)
+
+    def action_view_medical_history(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Medical History',
+            'res_model': 'veterinary.appointment',
+            'view_mode': 'tree,form,kanban',
+            'domain': [('pet_id', '=', self.id)],
+        }
+
+
+    def _compute_vaccinated(self):
+        for record in self:
+            record.vaccinated = bool(record.last_vaccination_date)
+    
+    def _inverse_vaccinated(self):
+        for record in self:
+            if record.vaccinated:
+                record.last_vaccination_date = fields.Date.today()
+            else:
+                record.last_vaccination_date = False
+
+    def _search_age(self, operator, value):
+        today = date.today()
+        if operator in ('=', '!='):
+            birth_year = today.year - value
+            start_date = date(birth_year, today.month, today.day)
+            end_date = date(birth_year - 1, today.month, today.day)
+            return [('birthdate', '>=', end_date), ('birthdate', '<=', start_date)]
+        elif operator in ('>', '>='):
+            birth_year = today.year - value
+            return [('birthdate', '<=', date(birth_year, today.month, today.day))]
+        elif operator in ('<', '<='):
+            birth_year = today.year - value
+            return [('birthdate', '>=', date(birth_year + 1, today.month, today.day))]
+        else:
+            return []
+
+    def _compute_age(self):
+        for record in self:
+            if record.birthdate:
+                record.age = (fields.Date.today() - record.birthdate).days // 365
+            else:
+                record.age = 0
+
+    def action_vaccinated(self):
+        for record in self:
+            record.vaccinated = True
+            record.last_vaccination_date = fields.Date.today()
+
+    def generate_pet_number(self):
+        for record in self:
+            record.pet_number = ''.join(random.choices('ABCDFG1234',k=8))
+    
+    def create_insurance(self):
+        self.env['veterinary.insurance'].create({'policy_number': '1234',
+                                                  'insurance_company': 'AXA',})
+    
+    def check_all_surgery_as_done(self):
+        surgeries = self.env['veterinary.surgery'].search([('pet_id', '=', self.id)])
+        surgeries.action_done()
+        
+    def action_print_appointments(self):
+        # appointments = self.env['veterinary.appointment'].search([('pet_id', '=', self.id)])
+        appointments = self.appointment_ids
+        report = self.env.ref('veterinary_clinic_nacho.action_report_veterinary_appointment').report_action(appointments.ids)
+        return report
