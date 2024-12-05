cf332c152f34861a619b225cd92c52964fe7ad0c
Borja <borja@somconnexio.coop>
2024-12-05 17:06:30 +0000

    insurance & sales

--- veterinary_clinic_sale_borja/models/sale_order.py
+++ veterinary_clinic_sale_borja/models/sale_order.py
@@ -6,18 +6,43 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     appointment_id = fields.Many2one("veterinary.appointment", string="Appointment")
+    insurance_count = fields.Integer(
+        string="Insurance Count", compute="_compute_insurance_count"
+    )
+
+    def _compute_insurance_count(self):
+        self.ensure_one()
+        self.insurance_count = len(
+            self.env["veterinary.insurance"].search([("order_id", "=", self.id)])
+        )
+
+    def action_insurance(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Insurances",
+            "res_model": "veterinary.insurance",
+            "view_mode": "tree,form",
+            "domain": [("order_id", "=", self.id)],
+        }
 
     def action_confirm(self):
         self.ensure_one()
         res = super().action_confirm()
+        # insurance_line = self.order_line.filtered(lambda self: self.product_id.insurance)
+        # if insurance_line:
+        #    self._create_insurance()
         for line in self.appointment_id.line_ids:
             if line.product_id.insurance:
-                insurance = {
-                    "pet_id": self.appointment_id.pet_id.id,
-                    "sale_id": self.id,
-                    "insurance_company": "Allianz",
-                    "policy_number": random.randint(1, 10000),
-                    "coverage_details": "Insurance from sale order",
-                }
-                self.env["veterinary.insurance"].create(insurance)
+                self._create_insurance()
         return res
+
+    def _create_insurance(self):
+        insurance = {
+            "pet_id": self.appointment_id.pet_id.id,
+            "order_id": self.id,
+            "insurance_company": "Allianz",
+            "policy_number": random.randint(1, 10000),
+            "coverage_details": "Insurance from sale order",
+        }
+        self.env["veterinary.insurance"].create(insurance)
